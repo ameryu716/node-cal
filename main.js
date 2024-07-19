f8b3550c5e286da3ff910747489f69355b6439db
Ameryu <72952772+ameryu716@users.noreply.github.com>
2024-07-19 17:10:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { ConsoleCalendar } from "./src/consoleCalendar.js";
 
 const [_engine, _process, ...args] = process.argv;
-const [year, month] = args
-const calendar = new ConsoleCalendar(year, month)
+const [year, month, is_holiday_data_refresh] = args
+const calendar = new ConsoleCalendar(year, month, Boolean(is_holiday_data_refresh))
 calendar.output()
