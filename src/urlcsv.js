4b38829c1ee8973740d4aac448c95d2d5ac4cade
Ameryu <72952772+ameryu716@users.noreply.github.com>
2024-07-19 17:12:10 +0900

    Update urlcsv.js

--- src/urlcsv.js
+++ src/urlcsv.js
@@ -1,3 +1,6 @@
+import * as fs from 'fs'
+// const fs = require('fs')
+
 export class URLCSV {
   async * #makeTextFileLineIterator(fileURL, charCode) {
     const utf8Decoder = new TextDecoder(charCode);
@@ -36,5 +39,37 @@ export class URLCSV {
     }
     // console.table(lines)
     return lines
+  }
+
+  async fetchAsJson(fileUrl, charCode, path) {
+    const holidayCsvData = await this.fetch(fileUrl, charCode)
+    holidayCsvData.shift()
+    const holidayDataObject = holidayCsvData
+      .reduce((result, line) => {
+        const [dateName, holidayName] = line.split(',')
+        result[dateName] = holidayName
+        return result
+      }, {})
+    fs.writeFileSync(path, JSON.stringify(holidayDataObject));
+  }
+
+  loadHolidayJson(path) {
+    const holidayJson = fs.readFileSync(path);
+    const holidayObject = JSON.parse(holidayJson)
+
+    const dateMap = Object.keys(holidayObject)
+      .map(key => {
+        const holidayName = holidayObject[key]
+        const [year, month, date] = key.split('/')
+
+        return [key, {
+          holidayName,
+          year: Number(year),
+          month: Number(month),
+          date: Number(date),
+        }]
+      })
+
+    return new Map(dateMap)
   }
 }
